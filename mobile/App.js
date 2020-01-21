8a87df68e8b3fe25b932ff773ea51797b358ca1e
Pedro Henrique Santana dos Santos <pedro.santana@ai4.com.br>
2020-01-21 14:44:43 -0300

    Aula 4 - Mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/router';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Chupa Minha Pica</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
+      <Routes />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
